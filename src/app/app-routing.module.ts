c280dff87766a8d3102d319a637455579e623e5a
Rafał Bujok <rafal.bujok@gmail.com>
2020-10-12 20:24:11 +0200

    add new manage component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ManageComponent } from './components/manage/manage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent
+  },
+  {
+    path: 'manage',
+    component: ManageComponent
+  },
+  {
+    path: '**',
+    component: DashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/manage/manage.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { StocksService } from 'src/app/services/stocks.service';
+
+
+@Component({
+  selector: 'app-manage',
+  templateUrl: './manage.component.html',
+  styleUrls: ['./manage.component.scss']
+})
+export class ManageComponent {
+
+  stock: string;
+  symbols: Array<string>;
+
+  constructor(private service: StocksService) {
+    this.symbols = service.get();
+  }
+
+  add() {
+    this.symbols = this.service.add(this.stock.toUpperCase());
+    this.stock = '';
+  }
+
+  remove(symbol) {
+    this.symbols = this.service.remove(symbol);
+  }
+
+
+}
